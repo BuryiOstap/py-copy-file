ec5c0c64d200143376608a6dc0545079b9cb6893
Ostap <ostap.burij@gmail.com>
2025-03-10 21:52:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(file_copy: str) -> None:
+    file_copy = file_copy.split()
+    if len(file_copy) == 3 and file_copy[0] == "cp":
+        try:
+            with (open(file_copy[1], "r") as current_file,
+                  open(file_copy[2], "w") as new_file):
+                new_file.write(current_file.read())
+        except FileNotFoundError:
+            print(f"File {file_copy[1]} not found.")
